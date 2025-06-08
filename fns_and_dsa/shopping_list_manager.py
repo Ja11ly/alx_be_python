63dd132f5bd1b8156431d9d69e5bce19d00eee63
Ja11ly <johnmaina2511@gmail.com>
2025-06-08 23:40:51 +0300

    Implement shopping list manager using Python lists

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -15,21 +15,21 @@ def main():
         if choice == '1':
             item = input("Enter the item to add: ")
             shopping_list.append(item)
-            print(f"'{item}' has been added to the list.")
+            print(f"'{item}' has been added.")
         elif choice == '2':
             item = input("Enter the item to remove: ")
             if item in shopping_list:
                 shopping_list.remove(item)
-                print(f"'{item}' has been removed from the list.")
+                print(f"'{item}' has been removed.")
             else:
                 print(f"'{item}' not found in the list.")
         elif choice == '3':
             if not shopping_list:
-                print("The shopping list is currently empty.")
+                print("Your shopping list is empty.")
             else:
                 print("Shopping List:")
-                for idx, item in enumerate(shopping_list, start=1):
-                    print(f"{idx}. {item}")
+                for i, item in enumerate(shopping_list, start=1):
+                    print(f"{i}. {item}")
         elif choice == '4':
             print("Goodbye!")
             break
