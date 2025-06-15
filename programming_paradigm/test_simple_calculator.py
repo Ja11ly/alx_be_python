e4a3b2dfe507ca54b96594b7c01f3563372f7c87
Ja11ly <johnmaina2511@gmail.com>
2025-06-15 21:38:30 +0300

    Fix: rename test methods to match checker expectations

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,25 +6,25 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_add(self):
+    def test_addition(self):  # MUST BE 'test_addition'
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
+    def test_subtraction(self):  # MUST BE 'test_subtraction'
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):
+    def test_multiply(self):  # MUST BE 'test_multiply'
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(0, 100), 0)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):
+    def test_divide(self):  # MUST BE 'test_divide'
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
-        self.assertEqual(self.calc.divide(5, 0), None)  # Edge case
+        self.assertEqual(self.calc.divide(5, 0), None)
 
 if __name__ == '__main__':
     unittest.main()
