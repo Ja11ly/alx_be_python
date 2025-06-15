a6964bad14cceb693d1638c4ed1888dd90a96404
Ja11ly <johnmaina2511@gmail.com>
2025-06-15 21:39:38 +0300

    Ensure test_multiply and test_divide follow checker requirements

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -6,25 +6,25 @@ class TestSimpleCalculator(unittest.TestCase):
     def setUp(self):
         self.calc = SimpleCalculator()
 
-    def test_addition(self):  # MUST BE 'test_addition'
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtraction(self):  # MUST BE 'test_subtraction'
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(-1, -1), 0)
         self.assertEqual(self.calc.subtract(0, 5), -5)
 
-    def test_multiply(self):  # MUST BE 'test_multiply'
+    def test_multiply(self):
         self.assertEqual(self.calc.multiply(3, 4), 12)
         self.assertEqual(self.calc.multiply(0, 100), 0)
         self.assertEqual(self.calc.multiply(-2, 3), -6)
 
-    def test_divide(self):  # MUST BE 'test_divide'
+    def test_divide(self):
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(-10, 2), -5)
-        self.assertEqual(self.calc.divide(5, 0), None)
+        self.assertEqual(self.calc.divide(5, 0), None)  # Division by zero edge case
 
 if __name__ == '__main__':
     unittest.main()
